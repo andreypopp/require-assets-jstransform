95220b9c76934eb4a09719add79c6312f9903d73
Andrey Popp <8mayday@gmail.com>
2014-02-18 00:42:54 +0400

    Fix specs

--- specs.js
+++ specs.js
@@ -6,6 +6,6 @@ describe('require-assets-jstransform', function() {
   it('works', function() {
     assert.equal(
       transform('1 + requireAssets("./index.js");'),
-      '1 + "/assets/index.js";');
+      '1 + "index.js";');
   });
 });
